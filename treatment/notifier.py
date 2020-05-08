0e8d6e7dbb8158f26c9bc84ebb27b0bc2bf04226
atchopba <atchopba@gmail.com>
2020-05-08 18:26:01 +0200

    Update notifier.py

--- treatment/notifier.py
+++ treatment/notifier.py
@@ -27,6 +27,12 @@ path_email = path_root + "email/" +cm.get_current_date_en() + "/"
 cm.create_folder(path_sms)
 cm.create_folder(path_email)
 
+def param_notif(notif, crp, p):
+    notif = notif.replace("#PERSONNE_DUE#", p.p_nom +" "+ p.p_prenom)
+    notif = notif.replace("#DATE_TEST#", p.date_test)
+    notif = notif.replace("#RELATION#", crp.relation)
+    notif = notif.replace("#PERSONNE_INFORMED#", crp.nom +" "+ crp.prenom)
+    return notif
 
 def notifier_personne(notification_id):
     
@@ -34,8 +40,6 @@ def notifier_personne(notification_id):
     
     # 1. selectionner les personnes avec le test positifs
     personnes_dict = Test().get_all("1")
-
-    print("=> personnes_dict => ", personnes_dict)
 
     # 2. pour chaque (1), selectionner les personnes avec qui il a été en "contact"
     for p in personnes_dict:
@@ -49,22 +53,21 @@ def notifier_personne(notification_id):
             
             # 3.1. envoyer un message pour le notifier 
             p_notif = cm.NOTIF.format(p.p_nom +" "+ p.p_prenom, p.date_test, crp.relation, crp.nom +" "+ crp.prenom)
-            print (p_notif)
+            #print (p_notif)
             
             # si un numéro de téléphone, un sms est envoyé
             if crp.num_telephone is not None and crp.num_telephone != "":
-                notif_ = notif_sms.format(p.p_nom +" "+ p.p_prenom, p.date_test, crp.relation, crp.nom +" "+ crp.prenom)
+                notif_ = param_notif(notif_sms, crp, p)
                 print(notif_)
                 cm.write_file(path_sms + str(crp.id) + ".txt", notif_)
             
             # si une @mail est rensigné, un email est envoyé
             if crp.email is not None and crp.email != "":
-                notif_ = notif_email.format(p.p_nom +" "+ p.p_prenom, p.date_test, crp.relation, crp.nom +" "+ crp.prenom)
+                notif_ = param_notif(notif_email, crp, p)
+                print("email\n : " + notif_)
                 cm.write_file(path_email + str(crp.id) + ".html", notif_)
                 # envoie du mail
                 cm.send_email(crp.email, notif_)
-            
-            # https://stackoverflow.com/questions/10147455/how-to-send-an-email-with-gmail-as-provider-using-python
             
             # 3.2. + enregistrement de la notification en base 
             pnotif = TPNotification(notification_id, crp.id, p.p_id, p_notif, cm.get_current_date_fr(), cm.get_current_time())
